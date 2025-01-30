34f548c2a109208094dc542270ffb4456e6a7016
codeWithMe <codewithhassan.404.not.found@gmail.com>
2025-01-30 17:26:33 +0100

    add project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,10 +3,41 @@ import imag from '../public/images/doctor.png'
 
 const ProjectCard = () => {
     return (
-       <div className="flex bg-neutral-800 flex-col max-w-[22rem] h-[25rem] p-1 overflow-hidden rounded-lg shadow-md">
-            <div>
-                <img src={imag} alt="" className="object-cover w-full h-full rounded-lg" />
-               </div>
+        <div className="card">
+            <div className="top-section">
+                <div className="borders"></div>
+                <div className="icons">
+                    <div className="logo">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 94 94" className="svg">
+                            <path fill="white" d="M38.0481 4.82927C38.0481 2.16214 40.018 0 42.4481 0H51.2391C53.6692 0 55.6391 2.16214 55.6391 4.82927V40.1401C55.6391 48.8912 53.2343 55.6657 48.4248 60.4636C43.6153 65.2277 36.7304 67.6098 27.7701 67.6098C18.8099 67.6098 11.925 65.2953 7.11548 60.6663C2.37183 56.0036 3.8147e-06 49.2967 3.8147e-06 40.5456V4.82927C3.8147e-06 2.16213 1.96995 0 4.4 0H13.2405C15.6705 0 17.6405 2.16214 17.6405 4.82927V39.1265C17.6405 43.7892 18.4805 47.2018 20.1605 49.3642C21.8735 51.5267 24.4759 52.6079 27.9678 52.6079C31.4596 52.6079 34.0127 51.5436 35.6268 49.4149C37.241 47.2863 38.0481 43.8399 38.0481 39.0758V4.82927Z"></path>
+                            <path fill="white" d="M86.9 61.8682C86.9 64.5353 84.9301 66.6975 82.5 66.6975H73.6595C71.2295 66.6975 69.2595 64.5353 69.2595 61.8682V4.82927C69.2595 2.16214 71.2295 0 73.6595 0H82.5C84.9301 0 86.9 2.16214 86.9 4.82927V61.8682Z"></path>
+                            <path fill="white" d="M2.86102e-06 83.2195C2.86102e-06 80.5524 1.96995 78.3902 4.4 78.3902H83.6C86.0301 78.3902 88 80.5524 88 83.2195V89.1707C88 91.8379 86.0301 94 83.6 94H4.4C1.96995 94 0 91.8379 0 89.1707L2.86102e-06 83.2195Z"></path>
+                        </svg>
+                    </div>
+                    <div className="social-media">
+                        <svg width="30" height="30" fill="#000" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" id="github">
+                            <path d="M12,2.2467A10.00042,10.00042,0,0,0,8.83752,21.73419c.5.08752.6875-.21247.6875-.475,0-.23749-.01251-1.025-.01251-1.86249C7,19.85919,6.35,18.78423,6.15,18.22173A3.636,3.636,0,0,0,5.125,16.8092c-.35-.1875-.85-.65-.01251-.66248A2.00117,2.00117,0,0,1,6.65,17.17169a2.13742,2.13742,0,0,0,2.91248.825A2.10376,2.10376,0,0,1,10.2,16.65923c-2.225-.25-4.55-1.11254-4.55-4.9375a3.89187,3.89187,0,0,1,1.025-2.6875,3.59373,3.59373,0,0,1,.1-2.65s.83747-.26251,2.75,1.025a9.42747,9.42747,0,0,1,5,0c1.91248-1.3,2.75-1.025,2.75-1.025a3.59323,3.59323,0,0,1,.1,2.65,3.869,3.869,0,0,1,1.025,2.6875c0,3.83747-2.33752,4.6875-4.5625,4.9375a2.36814,2.36814,0,0,1,.675,1.85c0,1.33752-.01251,2.41248-.01251,2.75,0,.26251.1875.575.6875.475A10.0053,10.0053,0,0,0,12,2.2467Z"></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom-section">
+                <span className="title">Still Cooking</span>
+                <div className="row row1">
+                    <div className="item">
+                        <span className="big-text">2</span>
+                        <span className="regular-text">UI Design</span>
+                    </div>
+                    <div className="item">
+                        <span className="big-text">100%</span>
+                        <span className="regular-text">Free to clone</span>
+                    </div>
+                    <div className="item">
+                        <span className="big-text">1</span>
+                        <span className="regular-text">Contributers</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
